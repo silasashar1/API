a1a8518273ca4a6ef271003f0e3f3c0c2a3521b6
silas@madstreetden.com <silas@madstreetden.com>
2025-01-31 14:07:54 +0530

    Added .spectral.mjs
    Committed by silasashar1 on behalf of silas@madstreetden.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3MTI5MQ/spectral.js?branch_slug=main&token=34732264-8829-4578-b8ce-41143766c940";
+export default { extends: ruleset };
